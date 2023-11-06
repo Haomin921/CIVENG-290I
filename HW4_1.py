d720b006c95c67074da744ade4e14aa6b14aac51
Haomin921 <149994286+Haomin921@users.noreply.github.com>
2023-11-06 00:52:39 -0800

    Update HW4_1.py

--- HW4_1.py
+++ HW4_1.py
@@ -1,42 +1,88 @@
 import heapq
+import numpy as np
+from scipy.io import loadmat
 
-def my_dijkstra(adj_matrix, origin):
+# Define the priority queue class using heapq
+class PriorityQueue:
+    def __init__(self):
+        self.heap = []
+        self.entry_finder = {}  # mapping of nodes to entries
+        self.REMOVED = '<removed-task>'  # placeholder for a removed task
+        self.counter = 0  # unique sequence count
+
+    def insert(self, node, priority):
+        if node in self.entry_finder:
+            self.remove(node)
+        count = self.counter
+        self.counter += 1
+        entry = [priority, count, node]
+        self.entry_finder[node] = entry
+        heapq.heappush(self.heap, entry)
+
+    def remove(self, node):
+        entry = self.entry_finder.pop(node)
+        entry[-1] = self.REMOVED  # Assign a placeholder for a removed task
+
+    def extract_min(self):
+        while self.heap:
+            priority, count, node = heapq.heappop(self.heap)
+            if node != self.REMOVED:
+                del self.entry_finder[node]
+                return node
+        raise KeyError('pop from an empty priority queue')
+
+    def decrease_key(self, node, priority):
+        # This will remove the node and re-insert it with the updated priority
+        self.remove(node)
+        self.insert(node, priority)
+
+    def is_empty(self):
+        return not self.entry_finder
+
+
+# Dijkstra's algorithm implementation in Python
+def myDijkstra(adj_matrix, origin):
     num_nodes = len(adj_matrix)
-    
-    # Initialize dist and prev arrays
     dist = [float('inf')] * num_nodes
-    prev = [None] * num_nodes
-    
-    # Initialize the distance to the origin node as 0
+    prev = [-1] * num_nodes
     dist[origin] = 0
     
-    # Create a priority queue and insert all nodes with their distances
-    pq = [(0, origin)]  # Priority queue as a list of tuples (distance, node)
-    visited = set()  # Set to keep track of visited nodes
+    pq = PriorityQueue()
+    for node in range(num_nodes):
+        pq.insert(node, dist[node])
     
-    while pq:
-        # Extract the node with the minimum distance
-        u_dist, u = heapq.heappop(pq)
-        
-        if u in visited:
-            continue
-        
-        visited.add(u)
-        
-        # Explore neighbors of u
+    while not pq.is_empty():
+        u = pq.extract_min()
         for v, weight in enumerate(adj_matrix[u]):
-            if weight > 0 and v not in visited:  # Check if there is an edge and v is unvisited
-                alt = u_dist + weight  # Calculate the potential new distance
-                if alt < dist[v]:  # If the new distance is shorter, update it
+            if weight > 0 and weight != np.inf:  # There is an edge
+                alt = dist[u] + weight
+                if alt < dist[v]:
                     dist[v] = alt
                     prev[v] = u
-                    heapq.heappush(pq, (alt, v))  # Update the priority queue
-    
-    # Set dist to infinity for unreachable nodes
-    dist = [d if d != float('inf') else float('inf') for d in dist]
-    
-    # Adjust prev to be 0-indexed
-    prev = [p if p is None else p + 1 for p in prev]
-    prev[origin] = origin
+                    pq.decrease_key(v, alt)
     
     return dist, prev
+
+# Load and process each graph
+num_graphs = 6
+
+for graph_num in range(1, num_graphs + 1):
+    # Load the graph data from the MAT file
+    file_name = f'graph{graph_num}.mat'
+    mat_data = loadmat(file_name)
+    adj_matrix = mat_data[f'graph{graph_num}']
+    
+    # Convert MATLAB 1-indexing to Python 0-indexing
+    
+    origin = 0  # Specify the origin node (starting node)
+    
+    # Call Dijkstra's algorithm
+    dist, prev = myDijkstra(adj_matrix, origin)
+    prev=[i+1 for i in prev]
+    
+    # Display the results
+    print(f'Table {graph_num}: {file_name}')
+    print('dist prev')
+    for i, (d, p) in enumerate(zip(dist, prev)):
+        print(f'{d} {p}')
+    print()
