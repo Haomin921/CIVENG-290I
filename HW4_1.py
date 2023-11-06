91c5918771ec9b2dbe11b2fa3a0f4d59a0bbef1a
Haomin921 <149994286+Haomin921@users.noreply.github.com>
2023-11-05 23:33:04 -0800

    Update HW4_1.py

--- HW4_1.py
+++ HW4_1.py
@@ -17,26 +17,26 @@ def my_dijkstra(adj_matrix, origin):
     while pq:
         # Extract the node with the minimum distance
         u_dist, u = heapq.heappop(pq)
-...         
-...         if u in visited:
-...             continue
-...         
-...         visited.add(u)
-...         
-...         # Explore neighbors of u
-...         for v, weight in enumerate(adj_matrix[u]):
-...             if weight > 0 and v not in visited:  # Check if there is an edge and v is unvisited
-...                 alt = u_dist + weight  # Calculate the potential new distance
-...                 if alt < dist[v]:  # If the new distance is shorter, update it
-...                     dist[v] = alt
-...                     prev[v] = u
-...                     heapq.heappush(pq, (alt, v))  # Update the priority queue
-...     
-...     # Set dist to infinity for unreachable nodes
-...     dist = [d if d != float('inf') else float('inf') for d in dist]
-...     
-...     # Adjust prev to be 0-indexed
-...     prev = [p if p is None else p + 1 for p in prev]
-...     prev[origin] = origin
-...     
-...     return dist, prev
+        
+        if u in visited:
+            continue
+        
+        visited.add(u)
+        
+        # Explore neighbors of u
+        for v, weight in enumerate(adj_matrix[u]):
+            if weight > 0 and v not in visited:  # Check if there is an edge and v is unvisited
+                alt = u_dist + weight  # Calculate the potential new distance
+                if alt < dist[v]:  # If the new distance is shorter, update it
+                    dist[v] = alt
+                    prev[v] = u
+                    heapq.heappush(pq, (alt, v))  # Update the priority queue
+    
+    # Set dist to infinity for unreachable nodes
+    dist = [d if d != float('inf') else float('inf') for d in dist]
+    
+    # Adjust prev to be 0-indexed
+    prev = [p if p is None else p + 1 for p in prev]
+    prev[origin] = origin
+    
+    return dist, prev
