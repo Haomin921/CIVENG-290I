5dac07583ee338b105e105cb71b4a0509d0bded0
Haomin921 <149994286+Haomin921@users.noreply.github.com>
2023-11-07 00:38:43 -0800

    Update MyDij.py

--- MyDij.py
+++ MyDij.py
@@ -1,46 +1,8 @@
 from flask import Flask
 import scipy.io
+from myDijkstra import my_dijkstra
 import io
 
-import heapq
-
-def my_dijkstra(adj_matrix, origin):
-    num_nodes = len(adj_matrix)
-    
-    # Initialize distance and previous arrays
-    dist = [float('inf')] * num_nodes
-    prev = [-1] * num_nodes
-
-    # Priority queue: (distance, node)
-    pq = [(0, origin)]
-    
-    # Set to keep track of visited nodes
-    visited = set()
-
-    # Initialize the distance of the origin node
-    dist[origin] = 0
-    
-    while pq:
-        # Extract the node with the minimum distance from the priority queue
-        curr_dist, u = heapq.heappop(pq)
-        
-        # If the node has already been visited, skip
-        if u in visited:
-            continue
-        
-        visited.add(u)
-
-        # Visit all neighbors of u
-        for v, weight in enumerate(adj_matrix[u]):
-            if weight > 0 and v not in visited:
-                alt = dist[u] + weight
-                if alt < dist[v]:
-                    # Update the distance to v and set the previous node
-                    dist[v] = alt
-                    prev[v] = u
-                    heapq.heappush(pq, (alt, v))
-                    
-    return dist, prev
 app = Flask(__name__)
 
 def main_script():
@@ -85,4 +47,4 @@ def run_script():
 
 if __name__ == '__main__':
     # Run the Flask app on all available interfaces on port 80
-    app.run(host='0.0.0.0', port=66)
+    app.run(host='0.0.0.0', port=69)
